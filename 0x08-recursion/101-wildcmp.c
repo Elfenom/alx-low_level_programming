9f811584e5501845ae5ccf06e6a4095bed824d59
Elfenom <elphenom10@gmail.com>
2024-07-10 11:17:16 -0700

    to be deleted

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,33 +1,76 @@
-nclude "main.h"
+#include "main.h"
 
 /**
- *  * str_checker - check if two strings are identical.
- *   * @s1: string_1 base address.
- *    * @s2: string_2 base address.
- *     * @i: left index.
- *      * @j: special index. (joker)
- *       * Return: 1 if s is palindrome, 0 otherwise.
- *        */
-int str_checker(char *s1, char *s2, int i, int j)
+ *  * check_sub_match - checks if the substring after * matches with s1
+ *   *
+ *    * @s1: base string
+ *     * @s2: string being compared
+ *      * @place: placeholder for index just after special char (*)
+ *       *
+ *        * Return: 1 if match, 0 otherwise
+ *         */
+
+int check_sub_match(char *s1, char *s2, char *place)
 {
-		if (s1[i] == '\0' && s2[j] == '\0')
-					return (1);
-			if (s1[i] == s2[j])
-						return (str_checker(s1, s2, i + 1, j + 1));
-				if (s1[i] == '\0' && s2[j] == '*')
-							return (str_checker(s1, s2, i, j + 1));
-					if (s2[j] == '*')
-								return (str_checker(s1, s2, i + 1, j) || str_checker(s1, s2, i, j + 1));
-						return (0);
+		if (*s1 == '\0' && *s2 == '\0')
+				{
+							return (1);
+								}
+
+			if (*s1 == '\0' && *s2 == '*')
+					{
+								return (check_sub_match(s1, s2 + 1, s2 + 1));
+									}
+
+				if (*s1 == '\0' && *s2 != '\0')
+						{
+									return (0);
+										}
+
+					if (*s2 == '*')
+							{
+										return (check_sub_match(s1, s2 + 1, s2 + 1));
+											}
+
+						if (*s1 == *s2)
+								{
+											return (check_sub_match(s1 + 1, s2 + 1, place));
+												}
+
+							else
+									{
+												return (check_sub_match(s1 + 1, place, place));
+													}
+
 }
+
+
 /**
- *  * wildcmp - check if strings could be considered identical
- *   * @s1: base address for string.
- *    * @s2: base address for string.
- *     *
- *      * Return: 1 if are considered identical.
- *       */
+ *  * wildcmp - compares 2 strings, one with special char '*'
+ *   *
+ *    * @s1: base string
+ *     * @s2: string being compared
+ *      *
+ *       * Return: '1' if match, '0' otherwise
+ *        */
+
 int wildcmp(char *s1, char *s2)
 {
-		return (str_checker(s1, s2, 0, 0));
+		if (*s1 == '\0' && *s2 == '\0')
+				{
+							return (1);
+								}
+
+			if (*s1 == *s2)
+					{
+								return (wildcmp(s1 + 1, s2 + 1));
+									}
+
+				else if (*s2 == '*')
+						{
+									return (check_sub_match(s1, (s2 + 1), (s2 + 1)));
+										}
+
+					else
+								return (0);
 }
